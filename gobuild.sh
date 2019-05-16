0d84b1918be435c95a02c7585b1c97c9d1e1f669
Olivier <Olivier123>
2019-05-17 01:08:30 +0200

    building

--- gobuild.sh
+++ gobuild.sh
@@ -1,1 +1,6 @@
-go build -buildmode=c-shared -o target/go/lib-prime.so \src\main\go\prime.go
+set PATH=C:\olivier.dugast\application\TDM-GCC-64\bin;%PATH%
+go build -buildmode=c-shared -o target/go/lib-prime.so \\main\go\prime.go
+go build -buildmode=c-shared -o target/go/lib-add.so src\main\go\Add.go
+c:\olivier.dugast\application\go\bin\go.exe
+
+runtime.main_main·f: function main is undeclared in the main package
